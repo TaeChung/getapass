1caa0092ae742389d1d6989358dfeacc89ed349a
Tae C <62630025+TaeChung@users.noreply.github.com>
2023-11-21 02:00:59 -0500

    attempted fixes

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function generatePassword (length, includeUppercase, includeNumbers, includeSymb
   const numberChars = '0123456789';
   const symbolChars = '!@#$%^&*()_+{}|:"<>?~';
 
-  letavailableChars = lowercaseChars;
+  let availableChars = lowercaseChars;
   if (includeUppercase) availableChars += uppercaseChars;
   if (includeNumbers) availableChars += numberChars;
   if (includeSymbols) availableChars += symbolChars;
@@ -19,28 +19,28 @@ function generatePassword (length, includeUppercase, includeNumbers, includeSymb
   }
   return password;
 }
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword(passwordLength, includeUppercase, includeSymbols, includeNumbers);
-  var passwordText = document.querySelector("password");
 
+
+function writePassword() {
+  // console.log('Initializing JavaScript function...');
+  // writePassword(25);
   const passwordLength = 25;
   const includeUppercase = true;
   const includeNumbers = true;
   const includeSymbols = true;
 
+  var password = generatePassword(passwordLength, includeUppercase, includeSymbols, includeNumbers);
+  var passwordText = document.querySelector("#password");
+
   passwordText.value = password;
 
 }
-
-
-
-// const password = generatePassword(25,true, true, true);
-// console.log(password);
-
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// const password = generatePassword(25,true, true, true);
