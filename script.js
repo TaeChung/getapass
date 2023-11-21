a865aa3990e6228dbdffb27d9efcb9890aaadda4
Tae C <62630025+TaeChung@users.noreply.github.com>
2023-11-20 23:32:00 -0500

    added function generatePassword and consts for characters

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // Assignment code here
+function generatePassword (length, includeUppercase, includeNumbers, includeSymbols) {
+  const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numberChars = '0123456789';
+  const symbolChars = '!@#$%^&*()_+{}|:"<>?~';
 
+  letavailableChars = lowercaseChars;
+  if (includeUppercase) availableChars += uppercaseChars;
+  if (includeNumbers) availableChars += numberChars;
+  if (includeSymbols) availableChars += symbolChars;
 
+const availableCharsLength = availableChars.length;
+  }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -11,7 +22,12 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+const password = generatePassword(20,true, true, true);
+console.log(password);
+
+}
